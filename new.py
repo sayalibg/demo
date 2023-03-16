148234c45f64721a7dc54d58a04473d2c0fa6bda
sayalibg <sayali251505@gmail.com>
2023-03-16 18:55:47 +0530

    demo project first commit

---
+++ new.py
@@ -0,0 +1,28 @@
+# print("hi")
+
+# print(__name__)   # __main__
+
+def func():
+    print("inside function")
+
+# # define main method:
+
+# if __name__ == "__main__":
+#     func()
+
+def main():
+    print("hi")
+
+    print(__name__)
+    func()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
---
+++ second.py
@@ -0,0 +1,5 @@
+import new
+
+print("hello")
+
+
